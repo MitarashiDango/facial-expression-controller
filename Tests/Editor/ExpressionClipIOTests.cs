087f2405aa8a75c492791e7efdfdef0ee3c64906
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2026-07-04 06:41:25 +0900

    ExpressionClipIO のテスト拡充 (#52)

--- Tests/Editor/ExpressionClipIOTests.cs
+++ Tests/Editor/ExpressionClipIOTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
@@ -128,6 +129,187 @@ namespace MitarashiDango.FacialExpressionController.Editor.Tests
             Assert.That(roundTripCurve.keys[1].inTangent, Is.EqualTo(-0.25f).Within(0.0001f));
         }
 
+        [Test]
+        public void LoadToClip_PreservesNonTargetFloatAndObjectReferenceCurves()
+        {
+            var renderer = CreateAvatarRenderer("Smile");
+            var sourceClip = new AnimationClip { name = "PreservedCurvesExpression" };
+            _temporaryObjects.Add(sourceClip);
+            sourceClip.SetCurve(
+                "Face",
+                typeof(SkinnedMeshRenderer),
+                "blendShape.Smile",
+                new AnimationCurve(new Keyframe(0f, 30f)));
+            sourceClip.SetCurve(
+                "Arm",
+                typeof(Transform),
+                "localPosition.x",
+                new AnimationCurve(new Keyframe(0f, 1f), new Keyframe(1f, 2f)));
+            sourceClip.SetCurve(
+                "OtherFace",
+                typeof(SkinnedMeshRenderer),
+                "blendShape.Smile",
+                new AnimationCurve(new Keyframe(0f, 77f)));
+            sourceClip.SetCurve(
+                "Face",
+                typeof(SkinnedMeshRenderer),
+                "blendShape.LegacyOnly",
+                new AnimationCurve(new Keyframe(0f, 15f), new Keyframe(1f, 25f)));
+
+            var spriteBinding = EditorCurveBinding.PPtrCurve("Icon", typeof(SpriteRenderer), "m_Sprite");
+            var sprite = CreateSprite("ExpressionIcon");
+            AnimationUtility.SetObjectReferenceCurve(
+                sourceClip,
+                spriteBinding,
+                new[]
+                {
+                    new ObjectReferenceKeyframe
+                    {
+                        time = 0f,
+                        value = sprite,
+                    },
+                });
+
+            var model = ExpressionClipIO.Load(sourceClip, renderer.transform.root.gameObject, renderer);
+            _temporaryObjects.Add(model);
+
+            Assert.That(model.preservedCurves, Has.Count.EqualTo(3));
+            Assert.That(model.preservedObjectReferenceCurves, Has.Count.EqualTo(1));
+
+            var roundTripClip = ExpressionClipIO.ToClip(model, "RoundTrip");
+            _temporaryObjects.Add(roundTripClip);
+
+            Assert.That(GetBlendShapeCurve(roundTripClip, "Face", "Smile"), Is.Not.Null);
+            var transformCurve = AnimationUtility.GetEditorCurve(
+                roundTripClip,
+                EditorCurveBinding.FloatCurve("Arm", typeof(Transform), "localPosition.x"));
+            Assert.That(transformCurve, Is.Not.Null);
+            Assert.That(transformCurve.keys[1].value, Is.EqualTo(2f).Within(0.0001f));
+
+            var otherBlendShapeCurve = GetBlendShapeCurve(roundTripClip, "OtherFace", "Smile");
+            Assert.That(otherBlendShapeCurve, Is.Not.Null);
+            Assert.That(otherBlendShapeCurve.keys[0].value, Is.EqualTo(77f).Within(0.0001f));
+
+            var legacyBlendShapeCurve = GetBlendShapeCurve(roundTripClip, "Face", "LegacyOnly");
+            Assert.That(legacyBlendShapeCurve, Is.Not.Null);
+            Assert.That(legacyBlendShapeCurve.keys[0].value, Is.EqualTo(15f).Within(0.0001f));
+            Assert.That(legacyBlendShapeCurve.keys[1].time, Is.EqualTo(1f).Within(0.0001f));
+            Assert.That(legacyBlendShapeCurve.keys[1].value, Is.EqualTo(25f).Within(0.0001f));
+
+            var objectReferenceKeys = AnimationUtility.GetObjectReferenceCurve(roundTripClip, spriteBinding);
+            Assert.That(objectReferenceKeys, Has.Length.EqualTo(1));
+            Assert.That(objectReferenceKeys[0].value, Is.SameAs(sprite));
+        }
+
+        [Test]
+        public void Load_IntermediateKeyWeightBlend_DetectsIntermediateKeysAndOnlyRewritesWhenEdited()
+        {
+            var renderer = CreateAvatarRenderer("Smile");
+            var sourceClip = new AnimationClip { name = "IntermediateKeyExpression" };
+            _temporaryObjects.Add(sourceClip);
+            var sourceCurve = new AnimationCurve(
+                new Keyframe(0f, 10f, 1.25f, 1.25f),
+                new Keyframe(0.5f, 60f, -0.5f, 0.75f),
+                new Keyframe(2f, 80f, -0.25f, -0.25f))
+            {
+                preWrapMode = WrapMode.ClampForever,
+                postWrapMode = WrapMode.PingPong,
+            };
+            sourceClip.SetCurve(
+                "Face",
+                typeof(SkinnedMeshRenderer),
+                "blendShape.Smile",
+                sourceCurve);
+
+            var model = ExpressionClipIO.Load(sourceClip, renderer.transform.root.gameObject, renderer);
+            _temporaryObjects.Add(model);
+
+            Assert.That(model.frameMode, Is.EqualTo(ExpressionFrameMode.WeightBlend));
+            Assert.That(model.hasIntermediateKeys, Is.True);
+            Assert.That(model.entries[0].value, Is.EqualTo(10f).Within(0.0001f));
+            Assert.That(model.entries[0].endValue, Is.EqualTo(80f).Within(0.0001f));
+
+            var unchangedClip = ExpressionClipIO.ToClip(model, "UnchangedRoundTrip");
+            _temporaryObjects.Add(unchangedClip);
+            var unchangedCurve = GetBlendShapeCurve(unchangedClip, "Face", "Smile");
+            Assert.That(unchangedCurve, Is.Not.Null);
+            Assert.That(unchangedCurve.length, Is.EqualTo(3));
+            Assert.That(unchangedCurve.preWrapMode, Is.EqualTo(WrapMode.ClampForever));
+            Assert.That(unchangedCurve.postWrapMode, Is.EqualTo(WrapMode.PingPong));
+            Assert.That(unchangedCurve.keys[0].time, Is.EqualTo(0f).Within(0.0001f));
+            Assert.That(unchangedCurve.keys[0].value, Is.EqualTo(10f).Within(0.0001f));
+            Assert.That(unchangedCurve.keys[0].outTangent, Is.EqualTo(1.25f).Within(0.0001f));
+            Assert.That(unchangedCurve.keys[1].time, Is.EqualTo(0.5f).Within(0.0001f));
+            Assert.That(unchangedCurve.keys[1].value, Is.EqualTo(60f).Within(0.0001f));
+            Assert.That(unchangedCurve.keys[2].time, Is.EqualTo(2f).Within(0.0001f));
+            Assert.That(unchangedCurve.keys[2].value, Is.EqualTo(80f).Within(0.0001f));
+            Assert.That(unchangedCurve.keys[2].inTangent, Is.EqualTo(-0.25f).Within(0.0001f));
+
+            model.entries[0].endValue = 90f;
+
+            var editedClip = ExpressionClipIO.ToClip(model, "EditedRoundTrip");
+            _temporaryObjects.Add(editedClip);
+            var editedCurve = GetBlendShapeCurve(editedClip, "Face", "Smile");
+            Assert.That(editedCurve, Is.Not.Null);
+            Assert.That(editedCurve.length, Is.EqualTo(2));
+            Assert.That(editedCurve.keys[0].time, Is.EqualTo(0f).Within(0.0001f));
+            Assert.That(editedCurve.keys[1].time, Is.EqualTo(1f).Within(0.0001f));
+            Assert.That(editedCurve.keys[0].value, Is.EqualTo(10f).Within(0.0001f));
+            Assert.That(editedCurve.keys[1].value, Is.EqualTo(90f).Within(0.0001f));
+        }
+
+        [Test]
+        public void ToClip_OutputModes_WriteExpectedBlendShapeCurves()
+        {
+            var renderer = CreateAvatarRenderer(
+                new[] { "Smile", "Blink", "Neutral" },
+                new[] { 0f, 0f, 0f });
+            var model = ExpressionClipIO.CreateModel(renderer.transform.root.gameObject, renderer);
+            _temporaryObjects.Add(model);
+            GetEntry(model, "Smile").value = 50f;
+            GetEntry(model, "Blink").value = 25f;
+
+            var referenceClip = new AnimationClip { name = "ReferenceExpression" };
+            _temporaryObjects.Add(referenceClip);
+            SetBlendShapeCurve(referenceClip, "Face", "Smile", new AnimationCurve(new Keyframe(0f, 50f)));
+            SetBlendShapeCurve(referenceClip, "Face", "Blink", new AnimationCurve(new Keyframe(0f, 0f)));
+            SetBlendShapeCurve(referenceClip, "Face", "Neutral", new AnimationCurve(new Keyframe(0f, 0f)));
+
+            var allTargetsClip = ExpressionClipIO.ToClip(model, "AllTargets");
+            _temporaryObjects.Add(allTargetsClip);
+            Assert.That(
+                GetBlendShapeCurveNames(allTargetsClip, "Face"),
+                Is.EquivalentTo(new[] { "Smile", "Blink", "Neutral" }));
+            AssertBlendShapeValue(allTargetsClip, "Face", "Smile", 50f);
+            AssertBlendShapeValue(allTargetsClip, "Face", "Blink", 25f);
+            AssertBlendShapeValue(allTargetsClip, "Face", "Neutral", 0f);
+
+            var sessionDiffClip = ExpressionClipIO.ToClip(
+                model,
+                "SessionDiff",
+                outputSettings: new ExpressionOutputSettings { mode = BlendShapeOutputMode.SessionBaselineDiff });
+            _temporaryObjects.Add(sessionDiffClip);
+            Assert.That(
+                GetBlendShapeCurveNames(sessionDiffClip, "Face"),
+                Is.EquivalentTo(new[] { "Smile", "Blink" }));
+            AssertBlendShapeValue(sessionDiffClip, "Face", "Smile", 50f);
+            AssertBlendShapeValue(sessionDiffClip, "Face", "Blink", 25f);
+
+            var referenceDiffClip = ExpressionClipIO.ToClip(
+                model,
+                "ReferenceDiff",
+                outputSettings: new ExpressionOutputSettings
+                {
+                    mode = BlendShapeOutputMode.ReferenceClipDiff,
+                    referenceClip = referenceClip,
+                });
+            _temporaryObjects.Add(referenceDiffClip);
+            Assert.That(
+                GetBlendShapeCurveNames(referenceDiffClip, "Face"),
+                Is.EquivalentTo(new[] { "Blink" }));
+            AssertBlendShapeValue(referenceDiffClip, "Face", "Blink", 25f);
+        }
+
         private static string GenerateAssetPath(string fileName)
         {
             return AssetDatabase.GenerateUniqueAssetPath($"Assets/{fileName}");
@@ -135,11 +317,24 @@ namespace MitarashiDango.FacialExpressionController.Editor.Tests
 
         private SkinnedMeshRenderer CreateAvatarRenderer(params string[] blendShapeNames)
         {
+            return CreateAvatarRenderer(blendShapeNames, null);
+        }
+
+        private SkinnedMeshRenderer CreateAvatarRenderer(IReadOnlyList<string> blendShapeNames, IReadOnlyList<float> weights)
+        {
             var avatarRoot = new GameObject("Avatar");
             var face = new GameObject("Face");
             face.transform.SetParent(avatarRoot.transform, false);
             var renderer = face.AddComponent<SkinnedMeshRenderer>();
             renderer.sharedMesh = CreateMesh(blendShapeNames);
+            if (weights != null)
+            {
+                for (var i = 0; i < weights.Count && i < blendShapeNames.Count; i++)
+                {
+                    renderer.SetBlendShapeWeight(i, weights[i]);
+                }
+            }
+
             _temporaryObjects.Add(renderer.sharedMesh);
             _temporaryObjects.Add(avatarRoot);
             return renderer;
@@ -180,6 +375,48 @@ namespace MitarashiDango.FacialExpressionController.Editor.Tests
             return AnimationUtility.GetEditorCurve(
                 clip,
                 EditorCurveBinding.FloatCurve(path, typeof(SkinnedMeshRenderer), $"blendShape.{blendShapeName}"));
+        }
+
+        private static BlendShapeEntry GetEntry(ExpressionEditModel model, string name)
+        {
+            return model.entries.First(entry => entry.name == name);
+        }
+
+        private static void SetBlendShapeCurve(
+            AnimationClip clip,
+            string path,
+            string blendShapeName,
+            AnimationCurve curve)
+        {
+            clip.SetCurve(path, typeof(SkinnedMeshRenderer), $"blendShape.{blendShapeName}", curve);
+        }
+
+        private static IEnumerable<string> GetBlendShapeCurveNames(AnimationClip clip, string path)
+        {
+            return AnimationUtility.GetCurveBindings(clip)
+                .Where(binding => binding.path == path
+                    && binding.type == typeof(SkinnedMeshRenderer)
+                    && binding.propertyName.StartsWith("blendShape."))
+                .Select(binding => binding.propertyName.Substring("blendShape.".Length));
+        }
+
+        private static void AssertBlendShapeValue(AnimationClip clip, string path, string blendShapeName, float expectedValue)
+        {
+            var curve = GetBlendShapeCurve(clip, path, blendShapeName);
+            Assert.That(curve, Is.Not.Null);
+            Assert.That(curve.length, Is.EqualTo(1));
+            Assert.That(curve.keys[0].time, Is.EqualTo(0f).Within(0.0001f));
+            Assert.That(curve.keys[0].value, Is.EqualTo(expectedValue).Within(0.0001f));
+        }
+
+        private Sprite CreateSprite(string name)
+        {
+            var texture = new Texture2D(1, 1) { name = $"{name}Texture" };
+            var sprite = Sprite.Create(texture, new Rect(0f, 0f, 1f, 1f), Vector2.zero);
+            sprite.name = name;
+            _temporaryObjects.Add(texture);
+            _temporaryObjects.Add(sprite);
+            return sprite;
         }
     }
 }
